4d790fdd6bfcd4bd784f2a3f6677a61083cd3c0e
502ermira <ermiraaa20@gmail.com>
2024-05-26 19:03:28 +0200

    Add single blog post retrieval

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const mongoose = require('mongoose');
 const Blog = require('./models/Blog');
 require('dotenv').config();
-const cors = require('cors');  
+const cors = require('cors');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.error(err));
 
-
 const corsOptions = {
   origin: '*', 
   methods: 'GET', 
-  allowedHeaders: 'Content-Type,Authorization', 
+  allowedHeaders: 'Content-Type,Authorization',
 };
 
 module.exports = async (req, res) => {
-
   cors(corsOptions)(req, res, async () => {
     if (req.method === 'GET') {
+      const { id } = req.query; 
       try {
-        const blogs = await Blog.find();
-        res.status(200).json(blogs);
+        if (id) {
+          const blog = await Blog.findById(id);
+          if (!blog) {
+            res.status(404).json({ error: 'Blog not found' });
+          } else {
+            res.status(200).json(blog);
+          }
+        } else {
+          const blogs = await Blog.find();
+          res.status(200).json(blogs);
+        }
       } catch (error) {
         res.status(500).json({ error: error.message });
       }
